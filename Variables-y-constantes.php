998c7ffd615237b82e5ce4103714fa51c8d8fe04
nuskacg <nuskadesigns@gmail.com>
2022-10-15 09:34:57 -0300

    Las variables y constantes permiten almacenar datos durante la ejecución de nuestro programa. El valor de una variable puede ser modificada, pero el de una constante no.

---
+++ Variables-y-constantes.php
@@ -0,0 +1,28 @@
+<?php
+
+//Una variables es como una cajita donde se guardan cosas.(Dentro de esa cajita podemos guardar cualquier cosa). Se pueden tener la cantidad de cajitas que uno quiera.
+//se pueden guardar y sacar y poner cosas una y otra vez.
+//Una constante no se puede cambiar.la cajita queda sellada. solo se puede ver lo que tiene dentro pero no se puede cambiar.
+
+//en php las variables se inician con un signo de dolar. 
+//$nombre = "Lore";
+//Para php cualquier cosa que este despues del signo $ va a ser un nombre.
+//la asignacion es lo que vamos a guardar dentro de la cajita llamada $nombre
+
+$numero_1 = 8;
+$numero_2 = 5;
+$nombre_1 = "Lore";
+
+echo $numero_1 + $numero_2;
+echo "\n";
+
+//para definir constantes en php usamos la funcion define.
+//es una buena practica poner los nombres de las constantes en mayuscula.
+
+define("NUMERO_PI", 3.14);
+echo NUMERO_PI;
+echo "\n";
+
+
+
+?>
