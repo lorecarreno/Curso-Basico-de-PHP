3dc84c5ba0f64ab5b4b4a599c73ac29cb5c4e9f1
nuskacg <nuskadesigns@gmail.com>
2022-10-15 17:15:58 -0300

    analisis de tipado en un php, resuelvo el ejercicio de los tipos de datos de una lista de variables

---
+++ análisis-de-tipado-en-PHP.php
@@ -0,0 +1,20 @@
+<?php
+
+//Nivel Facil
+
+$nombre = "Lore"; //String
+$apellido = "CG"; //String
+$edad = 18; //Integer
+$aprobado = true; //Boolean
+
+//Nivel Facil
+
+$promedio = (8 + 9.5 + 9 + 18 + 8) /5; //Decimal
+$nombre_completo = $nombre . " " . $apellido; //String
+$presento_examen = (bool) 1; //Boolean
+
+//Nivel Avanzado
+$numero_preguntas = 5 + "5"; //Integer
+$numero_respuestas = "5" + 5 //Integer
+$promerio_maximo = $numero_respuestas / 1.0; //Decimal
+$gatos = 3 + "5 gatos"; //Integer (warning)
