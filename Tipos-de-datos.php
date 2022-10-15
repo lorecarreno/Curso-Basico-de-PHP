1321340b5ac7f3ce92b4dd33af6462497168c938
nuskacg <nuskadesigns@gmail.com>
2022-10-15 10:00:28 -0300

    Un tipo de dato simplemente es una forma de diferenciar los datos que tenemos por medio de su tipo.

---
+++ Tipos-de-datos.php
@@ -0,0 +1,53 @@
+<?php
+
+/*Un tipo de dato simplemente es una forma de diferenciar los datos que tenemos por medio de su tipo. Por ejemplo, si es un número, una letra, una palabra, etc.*/
+
+//En programación podemos tener varios tipos de datos, pero los más conocidos son: numéricos, cadenas de caracteres, booleanos y sin valor
+
+//Numéricos : Representan a los datos de tipo numérico. Estos se pueden subdividir en:
+
+/*Integer: número sin decimales. 
+Por ejemplo: 50; 70; 300.
+
+/*Float: número con decimales. 
+Por ejemplo: 5.3; 4.7; 8.24.
+
+/*Double: número con decimales, 
+similar a float, pero con capacidad de almacenar más decimales. Especialmente útil para variables o constantes que requieren valores con mayor precisión, por ejemplo el número PI que tiene muchos decimales.
+
+//Cadenas de caracteres: Quiere decir que almacenaremos texto. Tenemos dos grupos:
+
+/*Char: es un solo caracter que puede ser una letra, número o un símbolo como por ejemplo #, @, %, a o 5.
+
+/*String: es una cadena de más de un caracter como por ejemplo “¡Hola, Mundo!”
+
+//Boolean: Es decir que algo es verdadero (true) o falso (false).
+
+//Sin valor: Sirve para expresar que una variable no tiene valor (Null) o que está no definido (Undefined)
+
+//El tipado de PHP
+/*PHP tiene tipado débil, es decir, no indicamos el tipo de variable al declararla, ya que puede deducir qué tipo de dato estás usando. Veamos un ejemplo:*/
+
+$numerito = 0;
+
+var_dump($numerito); //var_dump() lo usamos para ver la información de la variable, en este caso de $numerito.
+
+echo "\n"; //salto de linea.
+
+//Conversión de tipos automática
+/*PHP es capaz de convertir un tipo de dato a otro de forma automática según el contexto donde se emplee. Por ejemplo, si sumamos un número con un string, PHP evaluará ambos como números:*/ 
+
+$numerito = "23"; //String.
+$numerito = $numerito + 2 ; //Le sumamos al string un numero.
+
+var_dump($numerito); //var_dump() imprime la informacion.
+
+echo "\n"; //salto de linea.
+
+
+
+
+
+
+
+?>
--- Variables-y-constantes.php
+++ Variables-y-constantes.php
@@ -1,13 +1,15 @@
 <?php
 
-//Una variables es como una cajita donde se guardan cosas.(Dentro de esa cajita podemos guardar cualquier cosa). Se pueden tener la cantidad de cajitas que uno quiera.
-//se pueden guardar y sacar y poner cosas una y otra vez.
-//Una constante no se puede cambiar.la cajita queda sellada. solo se puede ver lo que tiene dentro pero no se puede cambiar.
+/*Una varible empieza simpre con un signo de $ y podemos cambiar su valor*
 
-//en php las variables se inician con un signo de dolar. 
-//$nombre = "Lore";
-//Para php cualquier cosa que este despues del signo $ va a ser un nombre.
-//la asignacion es lo que vamos a guardar dentro de la cajita llamada $nombre
+Una variables es como una cajita donde se guardan cosas.(Dentro de esa cajita podemos guardar cualquier cosa). Se pueden tener la cantidad de cajitas que uno quiera.
+se pueden guardar y sacar y poner cosas una y otra vez.
+Una constante no se puede cambiar.la cajita queda sellada. solo se puede ver lo que tiene dentro pero no se puede cambiar.
+
+en php las variables se inician con un signo de dolar. 
+$nombre = "Lore";
+Para php cualquier cosa que este despues del signo $ va a ser un nombre.
+La asignacion es lo que vamos a guardar dentro de la cajita llamada $nombre*/
 
 $numero_1 = 8;
 $numero_2 = 5;
@@ -17,6 +19,10 @@ echo $numero_1 + $numero_2;
 echo "\n";
 
 //para definir constantes en php usamos la funcion define.
+
+/*Una Constante es algo que nunca cambia, es decir, una vez que se le asigne un valor ya no es posible modificarlo ni sustituirlo.
+Una constante siempre mantendrá su valor intacto desde el momento en que se declara*/
+
 //es una buena practica poner los nombres de las constantes en mayuscula.
 
 define("NUMERO_PI", 3.14);
