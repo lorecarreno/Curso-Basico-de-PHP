7f2b642fe121d790e42c6c0de78b700ca2d12331
nuskacg <nuskadesigns@gmail.com>
2022-10-15 22:30:32 -0300

    Los operadores lógicos nos ayudan a combinar dos o más afirmaciones para definir si una oración es cierta o falsa. Su uso está basado en las tablas de verdad.

---
+++ operadores-en-php.php
@@ -0,0 +1,46 @@
+<?
+
+/*Operadores Lógicos
+Son los operadores que nos ayudan a combinar dos o mas afirmaciones para definir si una oración es cierta o falsa. Su uso esta basado en tablas de verdad.
+
+AND (y)
+Se usa para verificar si dos afirmaciones son ciertas, entonces la oración completa es cierta. Si una de ellas es falsa, entonces, la oración completa es falsa.
+
+true AND true = True
+
+false AND true = False
+
+true AND false = False
+
+false AND false = False
+
+Se escribe así:
+
+$valor_1 and $valor_2
+$valor_1 && $valor_2
+OR (o)
+Si una de las 2 afirmaciones es cierta, entonces la oración completa es cierta. Si solo una de ellas es falsa, entonces, la oración completa es cierta.
+
+true OR true = True
+
+false OR true = True
+
+true OR false = True
+
+false OR false = False
+
+Se escribe así:
+
+$valor_1 or $valor_2
+$valor_1 || $valor_2
+NOT (no)
+
+Se usa para invertir el significado de una oración
+
+NOT True ⇒ False
+
+NOT False ⇒ <True></True>
+
+
+
+?>
