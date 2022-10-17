95e4b5d5587447f12b920c36a4b7417c49254d96
nuskacg <nuskadesigns@gmail.com>
2022-10-17 18:42:45 -0300

    PHP también posee otros operadores importantes y muy usados como los de asignación, incremento/decremento y concatenación.

--- Otros-operadores-en-PHP.php
+++ Otros-operadores-en-PHP.php
@@ -2,24 +2,22 @@
 
 /* PHP también posee otros operadores importantes y muy usados como los de asignación, incremento/decremento y concatenación.
 
-Operador de asignación
+// Operador de asignación
 Representado por el signo = se usa para asignarle un valor determinado a una variable o constante:  
 
-<?php
-$a = 100;
-$b = 50 + 30;
+// $a = 100;
+// $b = 50 + 30;
 
 Este no se debe leer como “igual a” pues para expresar igualdad tenemos otros operadores como == (igual a) o === (idéntico a).
 
-Operadores de incremento/decremento
+// Operadores de incremento/decremento
 Estos operadores solo afectan a números y strings, pero no arrays, objects ni resources. Además, decrementar valores null tampoco tiene efecto, sin embargo, incrementarlos resulta en 1.
 
 Operador de incremento
 Representado por la secuencia de signos += son operadores utilizados para abreviar la forma en que incrementamos el valor de una variable. Veamos la siguiente línea de código:
 
-<?php
-$contador = 0;
-$contador = $contador + 10;
+// $contador = 0;
+// $contador = $contador + 10;
 
 echo $contador;
 
@@ -30,9 +28,8 @@ echo "\n";
 
 Esta puede ser simplificada usando un operador de incremento:
 
-<?php
-$contador = 0;
-$contador += 10;
+// $contador = 0;
+// $contador += 10;
 
 echo $contador;
 
@@ -73,12 +70,11 @@ echo "El contador 2 es igual a $contador_2 \n";
 Válido también para la multiplicación y división
 Estos operadores están representados por la secuencia de signos *= y /=:
 
-<?php
-$contador_1 = 5;
-$contador_1 *= 10;
+// $contador_1 = 5;
+// $contador_1 *= 10;
 
-$contador_2 = 1000;
-$contador_2 /= 2;
+// $contador_2 = 1000;
+// $contador_2 /= 2;
 
 echo "El contador 1 es igual a $contador_1 \n";
 echo "El contador 2 es igual a $contador_2 \n";
@@ -92,9 +88,10 @@ Operadores de concatenación
 Su funcionamiento es similar a los operadores de incremento/decremento.
 Son normalmente empleados para concatenar variables de tipo String con otras cadenas de caracteres.
 Es representado por la siguiente secuencia: .=.
-<?php
-$nombre = "Carlos";
-$nombre .= " " . "Santana";
+
+
+// $nombre = "Carlos";
+// $nombre .= " " . "Santana";
 # Esto es lo mismo que: $nombre = $nombre . " " . "Santana";
 
 echo $nombre;
