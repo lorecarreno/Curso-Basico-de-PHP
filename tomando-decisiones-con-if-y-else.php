4d635fffb94fc563cd3efc8dc06bada2e204ad8a
nuskacg <nuskadesigns@gmail.com>
2022-10-19 15:48:54 -0300

    La sentencia if y else es una gran forma de tomar decisiones cuando quiero comparar cosas

---
+++ tomando-decisiones-con-if-y-else.php
@@ -0,0 +1,18 @@
+<?php
+
+/* https://www.php.net/manual/es/control-structures.elseif.php */
+
+$asientos_disponibles = 0;
+$es_hijo_de_tom_holland = false;
+$conoce_a_tony_stark = true;
+
+if ($asientos_disponibles > 0)
+    echo "Puedes ver la pelicula de Spidey";
+else if ($es_hijo_de_tom_holland)
+    echo "No te creo, pero puedes ver la pelicula";
+else if ($conoce_a_tony_stark)
+    echo "Bueno, te creo, adelante";
+else
+    echo "Lo sentimos, te tocara spoilearte";
+
+echo "\n";
