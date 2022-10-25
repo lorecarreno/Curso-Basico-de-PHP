c1366591c0f7bb948bcb55af50f69bfa85031f28
nuskacg <nuskadesigns@gmail.com>
2022-10-25 20:56:14 -0300

    ciclo for

---
+++ ciclo-for.php
@@ -0,0 +1,31 @@
+<?php
+
+/*  
+
+ for ( *Parametros iniciales*;*Condiciones*;*Acciones*) {
+      //Este es el codigo que se repetira
+}
+
+/* for se usa mucho cuando estamos haciendo ciclos.
+entre parentesis hay tres secciones. Estan divididos por puntos y comas  */
+
+
+
+for ($contador = 0;$contador < 10; $contador++) {
+      // Escribir el codigo;
+      echo $contador  . "\n";
+}
+
+echo "\n";
+
+//este es un ciclo de manera inversa.
+
+for ($contador = 10; $contador > 0; $contador--) {
+      // Escribir el codigo;
+      echo $contador  . "\n";
+}
+
+echo "\n";
+
+// a la variable de $contador se le abrevia y llama de muchas formas pero la mas comun es i 
+
